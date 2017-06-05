52ec990a4df269c7607171e15331d0366c1ffb58
Frédéric DUPERIER <fduperier@gmail.com>
2017-06-05 11:08:32 +0200

    Services : Title

--- src/app/sub/sub.component.ts
+++ src/app/sub/sub.component.ts
@@ -1,16 +1,20 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { CustomService } from '../services/custom.service';
 import { SizeService } from '../services/size.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sub',
   templateUrl: './sub.component.html',
   styleUrls: ['./sub.component.css'],
-  providers: [ { provide: 'SizeService', useClass: CustomService } ]
+  providers: [ { provide: 'SizeService', useClass: CustomService } ],
+  viewProviders: [ Title ]
 })
 export class SubComponent implements OnInit {
 
-  constructor(@Inject('SizeService') private sizeService: SizeService) { }
+  constructor(@Inject('SizeService') private sizeService: SizeService, title: Title) {
+    title.setTitle('Sub component title');
+  }
 
   ngOnInit() {
     this.sizeService.run();
